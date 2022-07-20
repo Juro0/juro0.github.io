ef087e7607e2979080e1d3e526b1335a33ee7a3d
Juri <78843415+Juro0@users.noreply.github.com>
2022-07-20 10:02:51 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+const links = "/links"
+const bc = "/business-card"
+const cv = "/cv"
+const site = "/site"
+const error = "./404.html"
+const maintenance = "./maintenance.html"
+
+location.href = site
+
